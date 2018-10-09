3eb4c7605c9858b58efa4b27a96d1b46223c187a
Felipe Soares <altran.felipe@grupofleury.com.br>
2018-10-09 01:37:04 -0300

    Ajustado o projeto para rodar com bootstrap corretamente.

--- angular-desafio-fleury/e2e/src/app.e2e-spec.ts
+++ angular-desafio-fleury/e2e/src/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('angular-desafio-fleury App', () => {
+describe('workspace-project App', () => {
   let page: AppPage;
 
   beforeEach(() => {
@@ -9,6 +9,6 @@ describe('angular-desafio-fleury App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to ng4fbbootstrap!');
   });
 });
